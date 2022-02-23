69f74e98cf090377bcb5a05c0db406bad7941d9f
Masih <masihtamsoy@gmail.com>
2022-02-23 12:57:34 +0530

    v0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f173b2a4c4bc0d76cfb70024cf9f7b2b",
 "favicon.ico": "14f1e86b459447a87ea4ae1482f4d75c",
-"index.html": "327af7c03fb6265d6dd1c655af850217",
-"/": "327af7c03fb6265d6dd1c655af850217",
-"main.dart.js": "8ff696bf471f167b79148cbe9f26c75d",
+"index.html": "baf0e1fec17922f2135780478a318c3b",
+"/": "baf0e1fec17922f2135780478a318c3b",
+"main.dart.js": "9b2eafc95b762f7cc957ccc25d90f8b5",
 "icons/Icon-192.png": "b438a486c92ffea9200739bd3e9a9d0d",
 "icons/Icon-512.png": "90f5cf6171bd2452ebcc88d9fced312c",
 "manifest.json": "7e0bcd431993e3c5bc6cc0d9b070646b",
